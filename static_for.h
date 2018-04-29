9320a2d208d6022db4d9992b4a23fe8b55790f96
Valentin Galea <valentingalea@gmail.com>
2018-04-29 11:21:51 +0100

    remove warnings

--- static_for.h
+++ static_for.h
@@ -6,10 +6,15 @@ template<
 struct static_for
 {
 	template<class Func>
-	void operator ()(Func &&f) //TODO: constrain with enable_if callable
+	void operator ()(Func &&f) //TODO: constrain with `enable_if callable`
 	{
 		f(Begin);
+
+//TODO: a cleaner way is `if constexpr`
+#pragma warning(push)
+#pragma warning(disable:4127) // conditional expression is constant
 		if (Begin < End) {
+#pragma warning(pop)
 			static_for<Begin + 1, End>()(f);
 		}
 	}
@@ -19,7 +24,7 @@ template<int N>
 struct static_for<N, N>
 {
 	template<class Func>
-	constexpr void operator ()(Func &&f)
+	constexpr void operator ()(Func &&)
 	{
 	}
 };
