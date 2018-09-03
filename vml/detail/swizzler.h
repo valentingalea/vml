2f2aaa56d532d87c3fcbab570f3b90f00f30519b
Valentin Galea <valentingalea@gmail.com>
2018-09-03 17:47:11 +0100

    fix for get_vec_size

--- vml/detail/swizzler.h
+++ vml/detail/swizzler.h
@@ -6,9 +6,9 @@ namespace vml { namespace detail
 template<typename vector_type, typename T, size_t N, size_t... indices>
 struct swizzler
 {
-	static constexpr auto num_components = N;
+	static constexpr auto num_components = sizeof...(indices); // same as vector_type's
 
-	T data[N]; // N might differ from vector_type::num_components ex: .xxxx from vec3
+	T data[N]; // N might differ from num_components ex: .xxxx from vec3
 
 	vector_type decay() const
 	{
